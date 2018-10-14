6b7fb0341acb7449ecd79fda7b3bb67b486df340
THAWATCHAI SINGNGAM <merxer.io@gmail.com>
2018-10-14 22:24:09 +0700

    shadowing

--- rust/the-rust-programming-language/variables/src/main.rs
+++ rust/the-rust-programming-language/variables/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
+    let x = 5;
 
-    x = 6;
+    let x = x + 1;
+
+    let x = x * 2;
+
     println!("The value of x is: {}", x);
 }
